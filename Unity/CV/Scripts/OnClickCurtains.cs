279a84990228f1c62363aa4955dfb026dd87c68c
Maridany1999 <newartofuniverse@hotmail.com>
2022-01-24 14:56:14 +0000

    CV - Novos Audios e Interação com as Curtains

---
+++ Unity/CV/Scripts/OnClickCurtains.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JamCat.Cameras;
+
+public class OnClickCurtains : MonoBehaviour
+{
+    // Variables
+    public bool onClicked;
+    public AudioSource audioSource;
+
+    // Methods
+    void Update() {
+        UpdateRaycast();
+    }
+
+    void UpdateRaycast() {
+       if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = SysCamera.Get().getCurrentCamera().ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider != null) {
+                    if (hit.collider.tag == "cv_Curtains") {
+                        if (GetComponentInParent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Wind") == false) {
+                            audioSource.Play();
+                            GetComponentInParent<Animator>().SetTrigger("Wind");
+                        }             
+                    }
+                }
+            }
+        }
+    }
+}
